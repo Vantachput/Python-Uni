379d2e6da0ad613352b32d94ba9014089dbfbb35
Vantachput <73114387+Vantachput@users.noreply.github.com>
2023-09-18 16:39:35 +0300

    Update seecond.py

--- seecond.py
+++ seecond.py
@@ -1,4 +1,6 @@
 from math import sqrt
 print("Число"," ", "Квадратний корінь")
 for elem in range(100,0,-1):
-    print(elem, " ", sqrt(elem))
+    if(sqrt(elem) == int(sqrt(elem))):
+        print(" {elem:3} \t\x20\x20\x20\x20 {sqrt:3}".format(elem = elem, sqrt = int(sqrt(elem))))
+  
