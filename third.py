1c607861e46ef7dd7987eeccb5c13c3e6e1b41fb
Vantachput <73114387+Vantachput@users.noreply.github.com>
2023-09-18 16:39:46 +0300

    Update third.py

--- third.py
+++ third.py
@@ -2,9 +2,9 @@ n = int(input("Число від 1 до 9: "))
 while(n < 1 or n > 9):
     n = int(input("Число від 1 до 9: ")) 
 for i in range(0, n, 1):
-    print(" " * i, end = "")
+    print(" " * i * 2, end = "")
     for j in range(n-i, 0, -1):
         if(j == 1):
-            print(j, end='\n')
+            print("{j:1}".format(j=j), end='\n')
         else: 
-            print(j, end=' ') 
+            print("{j:1}".format(j=j), end=' ') 
